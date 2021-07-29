5db95fb1e5c9d716357b217b99873ee6ceeabea1
Martin Schibel <mshd94@gmail.com>
2021-07-29 22:12:22 +0800

    test draft

---
+++ src/lib/addPeoplePillImage.test.ts
@@ -0,0 +1,13 @@
+import addPeoplePillImage from "./addPeoplePillImage";
+import getWikidataEntities from "../wikidata/getWikidataEntities";
+import formatEntity from "./formatEntity";
+
+// describe("addPeoplePillImage", () => {
+//   test("it should give the correct name", async () => {
+//     const id = "Q47122";
+//     const res = await getWikidataEntities({ ids: [id] });
+//     const entity = formatEntity(res[id], "en");
+//     addPeoplePillImage(entity);
+//     expect(res[id].id).toBe("enrique-iglesias");
+//   });
+// });
