6ac8aac1e2ef394e38ae8ea737d00f6ba0ef2eef
Martin <mshd94@gmail.com>
2021-09-15 17:36:11 +0200

    update for factgrid

--- src/wikidata/getUpIds.ts
+++ src/wikidata/getUpIds.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import wdk from "wikidata-sdk";
+import { wikibaseInstance } from "lib/wikibaseInstance";
 
+const wdk = wikibaseInstance();
 export default async function getUpIds(
   entityId: string,
   propId: string,
 ): Promise<string[]> {
   const url = wdk.getReverseClaims(propId, entityId);
-
   const { data } = await axios.get(url);
 
   //TODO: get ids directly without simplify
