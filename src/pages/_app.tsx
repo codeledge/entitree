ae9421c9abca354251a8d18d03b2258e6aa71824
Orlando <orlando.groppo@gmail.com>
2021-09-29 00:50:49 +0200

    finally ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import store, { persistor, wrapper } from "../store";
 
 import { AppProps } from "next/app";
 import { GlobalStyle } from "layout/GlobalStyle";
-import Head from "next/head";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import React from "react";
@@ -23,14 +22,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-        <script async src="https://platform.twitter.com/widgets.js" />
-        <meta
-          name="keywords"
-          content="Family, Entity, Item, Tree, Taxonomy, Graph, Wikipedia, Wikidata, Diagram, Chart, D3, Hierarchy"
-        />
-      </Head>
       <GlobalStyle />
       <PersistGate loading={null} persistor={persistor}>
         {() => (
