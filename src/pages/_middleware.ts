1be312d14928b0760c83d38c58a85f47137c6d63
Orlando <orlando.groppo@gmail.com>
2021-10-28 10:32:09 +0100

    test host

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production")
     if (req.headers["x-forwarded-proto"] !== "https") {
       const host = req.headers.get("host");
+      console.log(req.headers);
+      console.log({ host });
+
       if (host) return NextResponse.redirect(`https://${host}${req.url}`);
     }
-  }
 }
