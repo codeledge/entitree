7da826817037e04f3bf47ce8080f83eee6d89512
Orlando <orlando.groppo@gmail.com>
2021-10-28 11:15:38 +0100

    check url struct

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  console.log(req);
+  console.log("URL", req.url);
   if (process.env.NODE_ENV === "production")
     if (req.headers.get("x-forwarded-proto") === "http") {
       const host = req.headers.get("host");
-      if (host) return NextResponse.redirect(`https://${host}${req.url}`);
+      if (host)
+        return NextResponse.redirect(`https://${host}${req.url.pathname}`);
     }
 }
