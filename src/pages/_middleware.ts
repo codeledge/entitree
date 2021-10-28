ed7c9e426ce9e6028c0df6f38da99385e7627194
Orlando <orlando.groppo@gmail.com>
2021-10-28 11:42:40 +0100

    try this

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,11 +1,11 @@
-import { NextURL } from "next/dist/server/web/next-url";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  console.log("URL", req.url as unknown as NextURL);
+  console.log("URL", req.nextUrl.pathname);
   if (process.env.NODE_ENV === "production")
     if (req.headers.get("x-forwarded-proto") === "http") {
       const host = req.headers.get("host");
-      if (host) return NextResponse.redirect(`https://${host}${req.url}`);
+      if (host)
+        return NextResponse.redirect(`https://${host}${req.nextUrl.pathname}`);
     }
 }
